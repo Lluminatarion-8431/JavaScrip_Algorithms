75759880c8af31ee57737b6b26806cc8159b38c3
Lluminatarion-8431 <Captain.a.8431@gmail.com>
2021-01-25 16:44:28 -0800

    Added a sorting algorithm of numbers bring sorted.

--- addNumbers.js
+++ addNumbers.js
@@ -98,5 +98,38 @@ function findMax(numbers) {
     return maximum
 }
 
-console.log(findMax([5, 2, 9, 8, 7, 3]));
-console.log(findMax([5, 8, 9, 8, 7, 3]));
+// console.log(findMax([5, 2, 9, 8, 7, 3]));
+// console.log(findMax([5, 8, 9, 8, 7, 3]));
+
+function sortBestRatingsFirst(numbers){
+    //sortBestRatingsFirst([5, 8, 2, 9, 3, 10])
+    // Know max AND it's index
+
+    // find max
+    for (let j = 0; j < numbers.length - 1; j++) {
+
+        let max_num = numbers[j];
+        let max_location = j;
+
+        for (let i = j; i < numbers.length; i++) {
+            if (numbers[i] > max_num) {
+                // Know max AND it's index(location)
+                max_num = numbers[i]
+                max_location = i
+            }
+        }
+
+        // swap the first and the last
+        numbers[max_location] = numbers[j]// => 10
+        numbers[j] = max_num
+    }
+
+    return numbers
+}
+
+console.log(sortBestRatingsFirst([5, 8, 2, 9, 3, 10]))
+console.log(sortBestRatingsFirst([9, 8, 1, 12, 3, 10]))
+console.log(sortBestRatingsFirst([5, 9, 8, 1, 12, 3, 10]))
+
+// Given a list of movies and their ratings
+// you need to sort the list in a descending order.
